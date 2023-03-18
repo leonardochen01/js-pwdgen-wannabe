b1ec3ac0d5f91a1ce6cf8757bf5db07386454bd3
leonardochen01 <leonardochen.lc@gmail.com>
2023-03-18 20:48:01 +0100

    added: password generator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 /*
 uso const (valore) per dare un valore fisso a nome, cognome e colore
 uso console.log per controllare che funzioni
-alla fine document.write per trascrivere le informazioni nel testo
 */ 
+
+const nome = prompt("Qual'è il tuo nome?")
+
+const cognome = prompt("Ed il tuo cognome?")
+
+const colore = prompt("Colore preferito?")
+
+console.log(nome , cognome , colore)
+
+/* 
+non riuscivo a usare document.write, quindi sono giunto a questa soluzione
+*/
+
+let output = document.getElementById("password").innerHTML = `Ciao ${nome}, questa è la tua password sicurissima: ${nome}${cognome}${colore}${Math.floor(Math.random() * 100)}`;
+
+/*Ho aggiunto un generatore randomico di numeri per rendere ancora più sicura la password*/
