e95e4d1209824e5d4f69ea3a59b973e16f5f5cf9
leonardochen01 <leonardochen.lc@gmail.com>
2023-03-18 20:27:30 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+uso const (valore) per dare un valore fisso a nome, cognome e colore
+uso console.log per controllare che funzioni
+alla fine document.write per trascrivere le informazioni nel testo
+*/ 
